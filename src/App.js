71585b991a8348dd43cc5dab74addaa5ab7d200b
fromaus-37 <naveed_ausaf@hotmail.com>
2023-05-31 11:10:10 +0100

    fix: get api working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const value = "Inara";
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState("loading...");
+
+  useEffect(() => {
+    (async function () {
+      await new Promise((resolveFuncForPromise) => {
+        const timeoutToken = setTimeout(() => {
+          clearTimeout(timeoutToken);
+          resolveFuncForPromise();
+        }, 6000);
+      });
+      const { text } = await (await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
